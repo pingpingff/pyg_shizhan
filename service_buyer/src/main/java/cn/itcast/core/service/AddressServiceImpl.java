a87226b26b5def30e72274c2060312d9bbaf8aff
ping <1126933225@qq.com>
2019-07-14 14:34:19 +0800

    12312

--- service_buyer/src/main/java/cn/itcast/core/service/AddressServiceImpl.java
+++ service_buyer/src/main/java/cn/itcast/core/service/AddressServiceImpl.java
@@ -3,13 +3,12 @@ package cn.itcast.core.service;
 import cn.itcast.core.dao.address.AddressDao;
 import cn.itcast.core.pojo.address.Address;
 import cn.itcast.core.pojo.address.AddressQuery;
-import com.alibaba.dubbo.config.annotation.Service;
-import org.springframework.beans.factory.annotation.Autowired;
+
 
 import java.util.List;
 
 @Service
-public class AddressServiceImpl implements AddressService {
+public class AddressServiceImpl implements cn.itcast.core.service.AddressService {
 
     @Autowired
     private AddressDao addressDao;
